4475ed9eae7ebfaaa6f4e646437b3284e10aa38f
Max Mezalon <jmezalon@gmail.com>
2021-12-30 17:59:00 -0500

    delete and show genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,5 @@
 class GenresController < ApplicationController
-    skip_before_action :authorize, only: [:index]
+    skip_before_action :authorize, only: [:index, :destroy]
 
     def create 
         render json: Genre.create!(genre_params), status: :created
@@ -7,6 +7,15 @@ class GenresController < ApplicationController
 
     def index 
         render json: Genre.all, status: :ok
+    end
+
+    def show 
+        render json: Genre.find(params[:id]), status: :ok
+    end
+
+    def destroy
+        Genre.find(params[:id]).destroy
+        head: :no_content
     end
 
     private 
